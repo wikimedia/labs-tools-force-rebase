95b126fb2dff7bdcd2d4cb72ba15b7ef0892799b
DannyS712 <dannys712.wiki@gmail.com>
2022-03-22 18:20:31 +0000

    Hello world - add index.php to public html
    Change-Id: I3aba1cd60029b20dcdf0cef5c32a8b12036d3d36

---
+++ public_html/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Main entry point
+ */
+
+echo "<html>";
+echo "<head>";
+echo "<title>Force rebase</title>";
+echo "</head>";
+echo "<body>";
+echo "<p>Hello, world!</p>";
+echo "</body>";
+echo "</html>";
